406852e0b6098774f7d49195f0bdbaf24ef72fcf
Emma Lübke <lubke.emma@gmail.com>
2024-11-06 13:05:39 +0100

    sista fixen

--- CaesarCipher/Program.cs
+++ CaesarCipher/Program.cs
@@ -1,4 +1,6 @@
-﻿namespace CaesarCipher
+﻿using System.ComponentModel.Design;
+
+namespace CaesarCipher
 {
     internal class Program
     {
@@ -21,25 +23,37 @@
 4. Exit
 
 Input choice: ");
-                int choice = int.Parse(Console.ReadLine());
+                string choice = (Console.ReadLine());
 
                 switch (choice)
                 {
-                    case 1:
+                    case "1":
                         Console.Clear();
                         Console.Write("Enter text to encrypt:  ");
                         string plaintext = Console.ReadLine();
 
-                        Console.Write("Enter shift key (e.g.,3): ");
-                        shift = int.Parse(Console.ReadLine());
 
-                        encryptedText = cipher.Encrypt(plaintext, shift);
-                        Console.WriteLine($"\nEncrypted Text : {encryptedText}");
-                        Console.WriteLine("Press any key to go back to meny");
-                        Console.ReadLine();
+                        while (true)
+                        {
+                            Console.Write("Enter shift key (e.g.,3): ");
+                            string input = (Console.ReadLine());
+
+                            if (int.TryParse(input, out shift) && shift >= 1 && shift <= 26)
+                            {
+                                encryptedText = cipher.Encrypt(plaintext, shift);
+                                Console.WriteLine($"\nEncrypted Text : {encryptedText}");
+                                Console.WriteLine("Press any key to go back to meny");
+                                Console.ReadLine();
+                                break;
+                            }
+                            else
+                            {
+                                Console.WriteLine("Shift must be between 1-26");
+                            }
+                        }
                         break;
 
-                        case 2:
+                    case "2":
                         Console.Clear();
                         string decryptedText = cipher.Decrypt(encryptedText, -shift);
                         Console.WriteLine($"\nDecrypted Text: {decryptedText}");
@@ -47,22 +61,22 @@ Input choice: ");
                         Console.ReadLine();
                         break;
 
-                        case 3:
+                    case "3":
                         Console.Clear();
                         Console.Write("Enter text to decrypt: ");
                         string encryptedNoKeyText = Console.ReadLine();
 
                         Console.WriteLine("Trying each possible key...");
-                        for(int i = 1; i <= 26; i++)
+                        for (int i = 1; i <= 26; i++)
                         {
                             string attemptDecryption = cipher.Decrypt(encryptedNoKeyText, i);
                             Console.WriteLine($"Key {i} : {attemptDecryption}");
                         }
-                            Console.WriteLine("Press any key to go back to meny");
-                            Console.ReadLine();
+                        Console.WriteLine("Press any key to go back to meny");
+                        Console.ReadLine();
                         break;
 
-                    case 4:
+                    case "4":
                         exit = true;
                         break;
 
@@ -72,32 +86,7 @@ Input choice: ");
 
                 }
             }
-
         }
-        //static string Encrypt(string text, int shift)
-        //{
-        //    return CaesarShift(text, shift);
-        //}
-
-        //static string Decrypt(string text, int shift)
-        //{
-        //    return CaesarShift(text, shift);
-        //}
-        //static string CaesarShift(string text, int shift)
-        //{
-        //    char[] buffer = text.ToCharArray();
-
-        //    for (int i = 0; i < buffer.Length; i++)
-        //    {
-        //        char letter = buffer[i];
-        //        if (char.IsLetter(letter))
-        //        {
-        //            char offset = char.IsUpper(letter) ? 'A' : 'a';
-        //            letter = (char)((((letter + shift) - offset + 26) % 26) + offset);
-        //        }
-        //        buffer[i] = letter;
-        //    }
-        //    return new string(buffer);
-        //}
     }
 }
+  
